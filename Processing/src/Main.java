5bbf4f83c56d515ddd487d03e2717e55def14e05
womyat <jannes.doege@manos-dresden.lernsax.de>
2021-01-14 22:27:10 +0100

    Processing

--- Processing/src/Main.java
+++ Processing/src/Main.java
@@ -6,16 +6,17 @@ public class Main extends PApplet {
         PApplet.main("Main", args);
     }
 
-    Textfeldd text = new Textfeldd();
+    Textfeld text = new Textfeld();
 
     @Override
     public void setup(){
-
+        background(60);
     }
 
     @Override
     public void draw(){
-        text.s_draw(1, 1);
+        background(60);
+        text.s_draw(20, 20);
     }
 
     @Override
@@ -25,19 +26,38 @@ public class Main extends PApplet {
 
     @Override
     public void keyPressed() {
-        if (key == '\n'){
-            text.typed = text.text;
-        }else {
-            text.text = text.text + key;
-        }
+        System.out.print(key);
+        text.s_type();
     }
 
-    class Textfeldd{
-        public  String text = "";
+    public static String remove_last_char(String str){
+        if (str.length() > 0){
+        return str.substring(0, str.length() -1);
+        }
+        return "";
+    }
+
+    public class Textfeld{
+        public boolean active = true;
+
+        public String text = "";
         public String typed = "";
 
-        public void s_draw(int xPos, int yPos){
+        public void s_draw(int xPos, int yPos) {
             text(text, xPos, yPos);
+        }
+
+        public void s_type(){
+            if (active){
+                if (key == '\n'){
+                    this.typed = this.text;
+                    this.text = this.text + key;
+                }else if(key == BACKSPACE){
+                    this.text = remove_last_char(this.text);
+                }else if (true){ //TODO filter out Shift, Space, ...
+                    this.text = this.text + key;
+                }
+            }
         }
     }
 }
